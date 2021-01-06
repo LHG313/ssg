7c4acf54d207a53b10e130a0c0998d9339d1254f
LHG313 <dd31391@gmail.com>
2021-01-06 23:39:34 +0900

    배포

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 function ArticleDetail__Body__init() {
-	if ( toastui === undefined ) {
+	if (toastui === undefined) {
 		return;
 	}
-	
+
 	var body = document.querySelector('.article-detail__body');
 	var initialValue = body.innerHTML.trim();
-	
+
 	var viewer = new toastui.Editor.factory({
-		el : body,
-		initialValue : initialValue,
-		viewer : true,
-	    plugins: [toastui.Editor.plugin.codeSyntaxHighlight]
+		el: body,
+		initialValue: initialValue,
+		viewer: true,
+		plugins: [toastui.Editor.plugin.codeSyntaxHighlight]
 	});
 }
 
